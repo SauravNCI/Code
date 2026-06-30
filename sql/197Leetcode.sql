89dd6db5f6e2eb4b59b7793374bf4c13535011b3
saurav chaudhary <55060238+SauravNCI@users.noreply.github.com>
2026-06-30 23:26:57 +0100

    Fix SQL syntax for weather temperature query

--- sql/197Leetcode.sql
+++ sql/197Leetcode.sql
@@ -2,4 +2,4 @@ with t1 as
 (select id ,temperature, recorddate,lag(recorddate,1) over(order by recorddate) as previousdate,lag(temperature,1) over(order by recorddate) as t
 from weather
 )
-select id from t1 where temperature>t and datediff(recorddate,previousdate)=1
+select id from t1 where temperature>t and datediff(recorddate,previousdate)=1;
