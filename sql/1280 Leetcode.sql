170a0ae607f51c70e092712061c87914fb103550
saurav chaudhary <55060238+SauravNCI@users.noreply.github.com>
2026-08-13 21:19:26 +0100

    update

--- sql/1280 Leetcode.sql
+++ sql/1280 Leetcode.sql
@@ -5,7 +5,7 @@ from students s join subjects ) t1 left join examinations e
 on t1.student_id = e.student_id and t1.subject_name =e.subject_name
 )
 , cte2 as(
-select student_id, student_name ,s1
+select student_id, student_name ,S1
 , case when s2 is null then 0 else 1 end as attended_exams
 from cte1)
 select student_id,student_name, s1 as subject_name , sum(attended_exams) as attended_exams
